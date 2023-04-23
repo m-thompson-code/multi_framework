c3ec856de132c786819c4fc70367196febcf6d2b
Eduard Krivanek <eduardkrivanek@Eduards-MacBook-Pro-2.local>
2023-04-23 14:43:00 +0300

    chore(vuejs): transition to modal

--- vuejs-3-example/src/components/shared/InputModal.vue
+++ vuejs-3-example/src/components/shared/InputModal.vue
@@ -1,34 +1,43 @@
 <template>
   <teleport to="#modal">
-    <div
-      v-show="prop.showModal"
-      ref="modal"
-      class="fixed inset-0 z-10 p-6 overflow-y-auto bg-black bg-opacity-50"
-      role="dialog"
-      aria-modal="true"
-      aria-labelledby="modal-headline"
+    <transition
+      enter-active-class="transition duration-300 ease-out transform "
+      enter-from-class="scale-95 translate-y-10 opacity-0"
+      enter-to-class="scale-100 translate-y-0 opacity-100"
+      leave-active-class="duration-200 ease-in"
+      leave-from-class="scale-100 translate-y-0 opacity-100"
+      leave-to-class="scale-95 translate-y-0 translate-y-10 opacity-0"
     >
-      <div class="p-4 min-w-[420px] max-w-[420px] bg-gray-400 rounded-lg mx-auto mt-[30%]">
-        <textarea v-model="input" placeholder="type value ..." class="w-full" />
+      <div
+        v-show="prop.showModal"
+        ref="modal"
+        class="fixed inset-0 z-10 p-6 overflow-y-auto bg-black bg-opacity-50"
+        role="dialog"
+        aria-modal="true"
+        aria-labelledby="modal-headline"
+      >
+        <div class="p-4 min-w-[420px] max-w-[420px] bg-gray-400 rounded-lg mx-auto mt-[30%]">
+          <textarea v-model="input" placeholder="type value ..." class="w-full" />
 
-        <div class="flex items-center gap-3 mt-4">
-          <button
-            type="button"
-            class="w-full p-2 text-center text-white bg-gray-600 rounded-lg"
-            @click="$emit('cancelClicked')"
-          >
-            Cancel
-          </button>
-          <button
-            type="submit"
-            class="w-full p-2 text-center text-white bg-green-600 rounded-lg"
-            @click="$emit('confirmClicked', input)"
-          >
-            Confirm
-          </button>
+          <div class="flex items-center gap-3 mt-4">
+            <button
+              type="button"
+              class="w-full p-2 text-center text-white bg-gray-600 rounded-lg"
+              @click="$emit('cancelClicked')"
+            >
+              Cancel
+            </button>
+            <button
+              type="submit"
+              class="w-full p-2 text-center text-white bg-green-600 rounded-lg"
+              @click="$emit('confirmClicked', input)"
+            >
+              Confirm
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    </transition>
   </teleport>
 </template>
 
