b0ec8587917da343f9c5380324183350a064b4c3
Eduard Krivanek <eduardkrivanek@Eduards-MacBook-Pro-2.local>
2023-04-26 09:26:49 +0300

    chore(vuejs): create reusable-template

--- vuejs-3-example/src/components/anime/AnimeDetails.vue
+++ vuejs-3-example/src/components/anime/AnimeDetails.vue
@@ -1,5 +1,17 @@
 <!-- eslint-disable vue/no-v-html -->
 <template>
+  <DefineTemplate v-slot="{ name, value }">
+    <div class="space-x-1">
+      <span
+        class="text-base"
+        :class="{ 'text-green-700': !props.animeData.isCool, 'text-yellow-700': props.animeData.isCool }"
+      >
+        {{ name }}
+      </span>
+      <span> {{ value }} </span>
+    </div>
+  </DefineTemplate>
+
   <article>
     <div class="flex gap-4 mb-2">
       <div v-html="renderHtml2('User', animeData.user.name)"></div>
@@ -9,26 +21,32 @@
       <div v-if="props.animeData.description" v-html="renderHtml2('Description', props.animeData.description)"></div>
     </div>
     <div class="flex gap-4">
-      <div v-html="renderHtml2('Episodes', props.animeData.selectedAnime.episodes)"></div>
+      <!-- <div v-html="renderHtml2('Episodes', props.animeData.selectedAnime.episodes)"></div> -->
+      <ReuseTemplate name="Episodes" :value="props.animeData.selectedAnime.episodes" />
 
       <div class="w-[2px] bg-gray-300"></div>
 
-      <div v-html="renderHtml2('Score', props.animeData.selectedAnime.score)"></div>
+      <!-- <div v-html="renderHtml2('Score', props.animeData.selectedAnime.score)"></div> -->
+      <ReuseTemplate name="Score" :value="props.animeData.selectedAnime.score" />
 
       <div class="w-[2px] bg-gray-300"></div>
 
-      <div v-html="renderHtml2('Duration', props.animeData.selectedAnime.duration)"></div>
+      <!-- <div v-html="renderHtml2('Duration', props.animeData.selectedAnime.duration)"></div> -->
+      <ReuseTemplate name="Duration" :value="props.animeData.selectedAnime.duration" />
 
       <div class="w-[2px] bg-gray-300"></div>
 
-      <div>{{ hardMathEquationMemo(props.animeData.selectedAnime.score) }}</div>
+      <div>Equation: {{ hardMathEquationMemo(props.animeData.selectedAnime.score) }}</div>
     </div>
   </article>
 </template>
 
 <script setup lang="ts">
+import { createReusableTemplate } from "@vueuse/core";
 import { hardMathEquationMemo } from "../../models/api-anime-data.model";
 import { AnimeTypeStore } from "../../store/anime.store";
+
+const [DefineTemplate, ReuseTemplate] = createReusableTemplate<{ name: string; value: string | number }>();
 
 const props = defineProps<{
   animeData: AnimeTypeStore;
